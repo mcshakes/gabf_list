f9c8e075bafe5315cfa649d1270203c926681d72
mcshakes <efmccarthy26@gmail.com>
2020-10-06 16:06:18 -0600

    Finally cleaned it up a bit

--- scrape.py
+++ scrape.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import csv
+import pandas as pd
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -38,14 +39,43 @@ soup = BeautifulSoup(page_source, "html.parser")
 table = soup.find("table", id="brewery-table")
 body = table.find("tbody")
 
+all_brewery_rows = list()
 
-with open("breweries.txt", mode="w", encoding='utf-8') as csv_file:
-    brewery_writer = csv.writer(
-        csv_file, delimiter=",")
+for data_row in body.find_all("tr"):
+    row_list = list()
 
-    for data_row in body.find_all("tr"):
-        name = [td.text for td in data_row.find_all("td.sorting_1")]
-        deal = [td.text for td in data_row.find_all("td.deal")]
-        brewery_writer.writerow([name])
+    single_brew_info = data_row.find(
+        "td", "sorting_1")
+
+    name_tag = single_brew_info.find("a").text  # COMPLETE
+
+    extra_data = single_brew_info.find("a").get(
+        "href").split("dir/")[0]
+
+    # if address_data[1]:
+    #     address_data = address_data[1].lstrip().split("+")
+    # else:
+    #     address_data = single_brew_info.find("a").get(
+    #         "href").split("dir/")
+
+    #     # import ipdb
+    #     # ipdb.set_trace()
+
+    # address = address_data[0]
+    # city = address_data[1]
+    # state = address_data[2]
+    # zipcode = address_data[3]
+
+    deal = data_row.find_all("td")[1].text
+
+    row_list.extend([name_tag, extra_data, deal])
+    all_brewery_rows.append(row_list)
+
+# print(all_brewery_rows)
+
+column_titles = ["Name", "Metadata", "Deal"]
+
+brew = pd.DataFrame(all_brewery_rows, columns=column_titles)
+brew.to_csv("breweries.csv")
 
 driver.quit()
